ecb22f6e4a30e4bb3d71baaab2f8a8460e07e8da
Dan Harris <danharris1606@gmail.com>
2019-01-09 14:05:57 +0000

    Add query endpoint
    Currently does it on groups only, but room to add further parameters

--- thing_directory/app.py
+++ thing_directory/app.py
@@ -20,7 +20,24 @@ def get_by_id(uuid):
         db_thing = Thing.get_by_uuid(uuid=uuid)
         return db_thing.get_json()
     except Exception as err:
-        return (json.dumps(err.message), 404, None)
+        return (json.dumps(err), 404, None)
+
+@app.route('/things/query', methods=['GET'])
+def query():
+    req_groups = request.args.get('groups').split(',')
+    s = shelve.open('things.db')
+    uuids = list(s.keys())
+    s.close()
+    matching = []
+    for id in uuids:
+        try:
+            thing = Thing.get_by_uuid(id)
+            groups = thing.get_groups()
+            if len(set(groups).intersection(req_groups)) > 0:
+                matching.append(thing.schema)
+        except Exception:
+            continue
+    return json.dumps(matching)
 
 @app.route('/things/register', methods=['POST'])
 def register():
