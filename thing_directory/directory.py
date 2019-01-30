25f317a89781187232e843cef955aee7439111ff
Dan Harris <danharris1606@gmail.com>
2019-01-30 10:28:11 +0000

    Separate flask builder and API endpoints
    To allow for flask testing

---
+++ thing_directory/directory.py
@@ -0,0 +1,86 @@
+from flask import Blueprint, current_app, request
+import shelve, json, uuid
+from thing import Thing
+
+bp = Blueprint('directory', __name__)
+
+@bp.route('/things', methods=['GET'])
+def get_all():
+    s = shelve.open(current_app.config['DB'])
+    ids = list(s.keys())
+    s.close()
+    response = {
+        "ids": ids
+    }
+    return json.dumps(response)
+
+@bp.route('/things/<uuid>', methods=['GET'])
+def get_by_id(uuid):
+    s = shelve.open(current_app.config['DB'])
+    try:
+        db_thing = Thing.get_by_uuid(s, uuid=uuid)
+        response = db_thing.get_json()
+    except Exception as err:
+        response = (str(err), 404, None)
+    finally:
+        s.close()
+        return response
+
+@bp.route('/things/query', methods=['GET'])
+def query():
+    req_groups = request.args.get('groups').split(',')
+    s = shelve.open(current_app.config['DB'])
+    uuids = list(s.keys())
+    matching = []
+    for id in uuids:
+        try:
+            thing = Thing.get_by_uuid(s, id)
+            groups = thing.get_groups()
+            if len(set(groups).intersection(req_groups)) > 0:
+                matching.append(thing.schema)
+        except Exception:
+            continue
+    s.close()
+    return json.dumps(matching)
+
+@bp.route('/things/register', methods=['POST'])
+def register():
+    # Needs to validate input
+    s = shelve.open(current_app.config['DB'])
+    new_thing = Thing(s, request.get_json())
+    new_thing.save()
+    response = {
+        "id": new_thing.uuid
+    }
+    s.close()
+    return (json.dumps(response), 201, None)
+
+@bp.route('/things/<uuid>/groups', methods=['POST'])
+def add_group(uuid):
+    body = request.get_json()
+    s = shelve.open(current_app.config['DB'])
+    try:
+        db_thing = Thing.get_by_uuid(s, uuid=uuid)
+    except Exception as err:
+        s.close()
+        return (str(err), 404, None)
+    db_thing.add_group(body['group'])
+    db_thing.save()
+    response = {
+        "message": "updated"
+    }
+    s.close()
+    return (json.dumps(response), 200, None)
+
+@bp.route('/things/<uuid>', methods=['DELETE'])
+def delete_thing(uuid):
+    s = shelve.open(current_app.config['DB'])
+    try:
+        db_thing = Thing.get_by_uuid(s, uuid=uuid)
+        db_thing.delete()
+        response = (json.dumps({"message": "deleted"}), 200, None)
+    except Exception as err:
+        response = (str(err), 404, None)
+    finally:
+        s.close()
+        return response
