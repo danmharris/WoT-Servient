5c2fe7b976c0bf80209fb581132874309bf523ce
Dan Harris <danharris1606@gmail.com>
2019-02-16 17:52:59 +0000

    Consolidate requirements.txt
    Was becoming far too confusing having one for each service, and
    dev dependencies were placed in there. As a result a single file
    now exists for all services, as well as a separate dev one that
    includes testing dependencies.

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 apt update && apt install python3-venv
 python3 -m venv /opt/wot-network
 source /opt/wot-network/bin/activate
-pip install Flask requests redis pyHS100
+pip install -r $(dirname $0)/requirements.txt
 python3 $(dirname $0)/setup.py install
 cp -r $(dirname $0)/etc/systemd/system/* /etc/systemd/system/
