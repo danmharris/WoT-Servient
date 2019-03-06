1a03ce3a3085ae83496ad6d45fa06d68b48c414b
Dan Harris <danharris1606@gmail.com>
2019-03-06 12:55:26 +0000

    Add index check on auth function
    If not passed in correct format will handle exception and return 401

--- src/common/auth.py
+++ src/common/auth.py
@@ -17,7 +17,10 @@ def check_auth():
     if current_app.config.get('AUTH') == True:
         header = request.headers.get('Authorization')
         if header is not None:
-            token = header.split(' ')[1]
+            try:
+                token = header.split(' ')[1]
+            except IndexError:
+                pass
             try:
                 jwt.decode(token, current_app.config.get('SECRET'), algorithm='HS256')
                 return
