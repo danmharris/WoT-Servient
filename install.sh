591097580519b25168b8d50e723c1e3328a96f4d
Dan Harris <danharris1606@gmail.com>
2019-03-27 14:02:24 +0000

    Fix dependencies in install script
    Had major issues with aiocoap installation, requiring additional
    dependencies. These have now been added

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-apt update && apt install python3-venv
+apt update && apt install -y git python3-venv python3-dev autoconf build-essential libffi-dev libssl-dev redis-server
 python3 -m venv /opt/wot-network
 source /opt/wot-network/bin/activate
+pip install wheel
 pip install -r $(dirname $0)/requirements.txt
-python3 $(dirname $0)/setup.py install
+pip install $(dirname $0)
 cp -r $(dirname $0)/etc/systemd/system/* /etc/systemd/system/
 cp $(dirname $0)/etc/wot-network.ini /etc/
--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ setup(
     version="0.1",
     package_dir={'':'src'},
     packages=find_packages('src', exclude=['*.tests']),
-    install_requires=['Flask>=1.0.2', 'requests>=2.21.0', 'redis>=3.1.0', 'pyHS100>=0.3.4'],
+    install_requires=[
+        'Flask>=1.0.2',
+        'requests>=2.21.0',
+        'redis>=3.1.0',
+        'pyHS100>=0.3.4',
+        'PyJWT==1.7.1',
+        'Click==7.0',
+        ],
     author="Dan Harris",
     description="Set of services used to build a network based on W3 specification",
     entry_points={
