36b0e188c7b148d0dca414218908059cd4b91709
Dan Harris <danharris1606@gmail.com>
2019-04-22 22:59:48 +0100

    Fix lint errors in common module

--- src/common/db.py
+++ src/common/db.py
@@ -1,12 +1,15 @@
+"""This module contains the ability to create shelve database in Flask"""
 import shelve
 from flask import current_app, g
 
 def get_db():
+    """Get the DB object, creating it if not yet open"""
     if 'db' not in g:
         g.db = shelve.open(current_app.config['DB'])
     return g.db
 
 def close_db(e=None):
+    """Close the DB object"""
     db = g.pop('db', None)
     if db is not None:
         db.close()
