f17eba423a9b0f91f7d21c742dcb71f91da27498
Dan Harris <danharris1606@gmail.com>
2020-04-18 18:15:50 +0100

    Update pyHS100

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'aiocoap[all]==0.4b1',
         'Click==7.0',
         'requests>=2.21.0',
-        'pyHS100>=0.3.4',
+        'pyHS100>=0.3.5',
         ],
     author="Dan Harris",
     description="Common libraries to be used by microservices in the WoT Servient",
