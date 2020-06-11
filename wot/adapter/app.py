b6186176e38211a1b9189ed7a15960d08843f34f
Dan Harris <danharris1606@gmail.com>
2020-06-11 20:16:12 +0100

    Add config to adapters
    Decided to use yaml going forward, nicer (and safer) than using python
    files. Install to /opt/wot/config/, venv has moved to /opt/wot/venv/.
    
    Also uninstall services on make uninstall

--- wot/adapter/app.py
+++ wot/adapter/app.py
@@ -1,16 +1,27 @@
+
+import os.path
 from flask import Flask, Blueprint, jsonify
+import yaml
 import wot.adapter.device.tplink
 
-def create_app():
+CONFIG_LOCATION = '/opt/wot/config/adapter.yaml'
+
+def create_app(app_config=None):
     app = Flask(__name__)
 
-    device_bp = Blueprint('devices', __name__, '/devices')
+    if app_config is None:
+        app.config.from_object('wot.adapter.config')
+        if os.path.exists(CONFIG_LOCATION):
+            with open(CONFIG_LOCATION, 'r') as stream:
+                app.config.from_mapping(yaml.safe_load(stream))
+
     schemas = list()
 
-    devices = wot.adapter.device.tplink.get_things()
-    for device in devices:
-        app.register_blueprint(device.blueprint)
-        schemas.append(device.schema)
+    if 'tplink' in app.config['ENABLED']:
+        devices = wot.adapter.device.tplink.get_things()
+        for device in devices:
+            app.register_blueprint(device.blueprint)
+            schemas.append(device.schema)
 
     @app.route('/things', methods=['GET'])
     def things():
